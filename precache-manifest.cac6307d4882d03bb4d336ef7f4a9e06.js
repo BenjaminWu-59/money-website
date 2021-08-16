a3f0c6a0d530b2fbe622820c3f3f0bbdec762b98
Benjamin <wuqiong3699@gmail.com>
2021-08-16 16:53:18 +0800

    deploy

---
+++ precache-manifest.cac6307d4882d03bb4d336ef7f4a9e06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9e47eb8f07037c23d7",
+    "url": "/money-website/css/app.d8600589.css"
+  },
+  {
+    "revision": "8947740c5c86e5830228d0b93f244f27",
+    "url": "/money-website/img/404.8947740c.jpg"
+  },
+  {
+    "revision": "f0e3baaf32b0c644dad9c4450d2173c2",
+    "url": "/money-website/img/rain4.f0e3baaf.jpeg"
+  },
+  {
+    "revision": "f1184d867180ba4c7d5848d97102950f",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "0b9e47eb8f07037c23d7",
+    "url": "/money-website/js/app.ed8f5426.js"
+  },
+  {
+    "revision": "2b7c62ed679aa9e22ca4",
+    "url": "/money-website/js/chunk-vendors.0ab75b64.js"
+  },
+  {
+    "revision": "38223f382ae68db0b789beafa5bf680b",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
